023439e1eaa6f4dfd0903cf8bb7d25ab80bf10d7
Sourabh Malviya <saurabhmalvia997@gmail.com>
2021-07-15 17:05:43 +0530

    Adding doc

--- src/com/sourabh/db/dao/SalesRecordDao.java
+++ src/com/sourabh/db/dao/SalesRecordDao.java
@@ -6,7 +6,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface SalesRecordDao {
+    /**
+    * Insert record in database
+    */
     SalesRecord insertRecord(SalesRecord record);
 
+    /**
+    * Reterive all the sales record from database between start-date and end-date
+    */
     List<SalesRecord> getSalesRecordBetweenDate(LocalDate startDate, LocalDate endDate);
 }
